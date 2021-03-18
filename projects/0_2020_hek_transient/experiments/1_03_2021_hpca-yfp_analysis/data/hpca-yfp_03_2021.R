c81a9a94049c1aada9cd4f3df1e6f4591babaf47
wisstock <omnia.fatum@gmail.com>
2021-03-19 01:41:34 +0200

    add HPCA-YFP 03.2021 results

--- projects/0_2020_hek_transient/experiments/1_03_2021_hpca-yfp_analysis/data/hpca-yfp_03_2021.R
+++ projects/0_2020_hek_transient/experiments/1_03_2021_hpca-yfp_analysis/data/hpca-yfp_03_2021.R
@@ -16,26 +16,26 @@ df.fluo <- read.csv('results_fluo.csv') %>%  # 20.10.2020, results of test uncag
            summarise_all(list(mean, sd), na.rm = TRUE) %>%
            rename(mean = fn1, sd = fn2, frame = time)
 df.fluo$time <- as.numeric(df.fluo$frame) - 4
-write.csv(df.fluo, 'fluo_4.csv')
 
-df.yfp <- rbind(read.csv('results_10.csv'), read.csv('results_20.csv'))
-write.csv(df.yfp, 'hpca_yfp.csv')
-df.mask <- subset(df.yfp,
-                  ((mask == 'up') & (cell == 'cell2_01')) |
-                  ((mask == 'up') & (cell == 'cell7_01')))
+df.yfp <- read.csv('results_17.csv') %>%  # results 17.03.2021, 20th frame stimulation
+          select(-c('rel')) %>%
+          rbind(read.csv('results_10.csv'),  # results 22,27.01.2021, 10th frame stimulation
+          read.csv('results_20.csv')) #,  # results 22,27.01.2021, 20th frame stimulation
+          
+df.mask <- subset(df.yfp, mask == 'up')
 
 
 coeff <- 10
 ggplot() +
   geom_line(data=df.mask,
             aes(x=time, y=delta, colour=as.factor(cell)),
-            size=1) +
+            size=0.8) +
   geom_line(data=subset(df.fluo, (power == 20)),
             aes(x=time, y=mean/coeff, colour=as.factor(power)),
-            size=1,
+            size=0.5,
             linetype=5) +
   scale_x_continuous(name='Time (s)',
-                     limits=c(-20, 120),
+                     limits=c(-10, 10),
                      breaks=seq(-100, 100, 10)) +
   scale_y_continuous(name='HPCA-YFP ΔF/F0',
                      sec.axis=sec_axis(trans=~.*coeff,
