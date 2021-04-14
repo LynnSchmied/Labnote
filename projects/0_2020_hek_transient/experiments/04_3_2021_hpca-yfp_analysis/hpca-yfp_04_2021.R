db95b79f868dfa6ae2997b359dba03cd562e4fbc
wisstock <omnia.fatum@gmail.com>
2021-04-14 23:15:48 +0300

    ctrl commit

--- projects/0_2020_hek_transient/experiments/04_3_2021_hpca-yfp_analysis/hpca-yfp_04_2021.R
+++ projects/0_2020_hek_transient/experiments/04_3_2021_hpca-yfp_analysis/hpca-yfp_04_2021.R
@@ -1,5 +1,19 @@
 # HPCA-EYFP data analysis, uncaging with different stimulation power
 # Copyright © 2021 Borys Olifirov
+#
+# Data frame structure:
+# 'ID'     full unice recording ID
+# 'date'   experiment date
+# 'cell'   cell number with date suffix
+# 'rep'    number of stimulation for this cell
+# 'power'  405 nm laser power
+# 'stimul' number of frame directly before stimulation
+# 'frame'  number of frame
+# 'time'   absolute time of frame recording, stimulation frame used as 0
+# 'mask'   mask type
+# 'int'    mean intensity of mask
+# 'delta'  ΔF/F0 for int data
+# 'rel'    ΔF/F0 for mask/cell mask, cell photo bleaching compensation
 
 require(dplyr)
 require(tidyr)
@@ -21,7 +35,7 @@ df.fluo <- read.csv('results_fluo.csv') %>%
            mutate(time = frame - 3) %>%
            mutate(power = as.factor(power))
 
-df.yfp <- rbind(read.csv('results_3_04.csv')) %>%  # ,read.csv('results_17_03.csv')
+df.yfp.04 <- rbind(read.csv('results_3_04.csv')) %>%  # ,read.csv('results_17_03.csv')
           mutate(power = as.factor(power)) %>%
           mutate(cell = as.factor(cell)) %>%
           mutate(date = as.factor(date)) %>%
@@ -35,75 +49,74 @@ df.yfp.03 <- read.csv('results_17_03.csv') %>%
              mutate(rep = as.factor(rep)) %>%
              subset(select = c(ID, cell, date, rep, power, time, mask, delta, rel))
 
+# DROP BAD CELLS
+df.yfp.04.drop <- df.yfp.04 %>%
+  filter(ID == 'cell1_01_3_04' |  # 405 nm 50%
+           ID == 'cell1_02_3_04' |
+           ID == 'cell1_03_3_04' |
+           ID == 'cell10_01_3_04'|
+           ID == 'cell4_01_3_04' |
+           ID == 'cell7_01_3_04' |
+           ID == 'cell2_01_3_04' |  # 405 nm 75%
+           ID == 'cell2_02_3_04' |
+           ID == 'cell2_03_3_04' |
+           ID == 'cell8_01_3_04' |
+           ID == 'cell8_02_3_04' |
+           ID == 'cell8_03_3_04' |
+           ID == 'cell3_01_3_04' |  # 405 nm 100%
+           ID == 'cell3_02_3_04' |
+           ID == 'cell6_01_3_04' |
+           ID == 'cell9_01_3_04')
+
+df.yfp.03.drop <- df.yfp.03 %>%
+  filter(ID == 'cell7_01_17_03'  |  # 405 nm 20%
+           ID == 'cell9_01_17_03'  |
+           ID == 'cell10_01_17_03' |  # 405 nm 50%
+           ID == 'cell12_01_17_03' |
+           ID == 'cell13_01_17_03' |
+           ID == 'cell20_01_17_03') 
+# ALL GOOD CELLS               
+df.yfp <- rbind(df.yfp.04.drop, df.yfp.03.drop)        
+
+
 ##### RAW #####
 # REPEATED STIMULATIONS
 selected.power <- '50'
 selected.mask <- 'up'
 
 df.power.norm <- df.yfp %>%
-                 filter(mask == selected.mask,
-                        power == selected.power) %>%
-                 subset(select = c(ID, cell, rep, time, delta)) %>%
-                 group_by(cell, rep) %>%
-                 mutate(delta_norm = delta / max(delta))
+                 filter(power == selected.power,
+                        mask == selected.mask) %>%
+                 subset(select = c(ID, date, cell, mask, rep, time, rel)) %>%
+                 group_by(cell, rep, mask) %>%
+                 mutate(delta_norm = rel / max(rel))
 
-# 17.03.2021 data
-df.power.norm.03 <- df.yfp.03 %>%
-  filter(mask == selected.mask,
-         power == selected.power) %>%
-  subset(select = c(ID, cell, rep, time, delta)) %>%
-  group_by(cell, rep) %>%
-  mutate(delta_norm = delta / max(delta))
-
-ggplot(data = df.power.norm.03) +
+ggplot(data = df.power.norm) +
   geom_vline(xintercept = 0) +
-  geom_line(aes(x=time, y= delta_norm, colour=rep),
+  geom_line(aes(x=time, y= delta_norm, colour=ID),
             size=1.2) +
-  facet_grid(rows = vars(cell)) +
   scale_x_continuous(name = 'Time (s)',
                     limits = c(-20, 90),
                    breaks = seq(-100, 100, 5)) +
   scale_y_continuous(name = 'Normalized ΔF/F0',
-                     breaks = seq(-100, 100, 10)) +
+                     limits = c(-0.5, 1),
+                     breaks = seq(-100, 100, 0.5)) +
   labs(title = sprintf("Mask %s, 405 nm power %s", selected.mask, selected.power),
        colour = 'Stimulation') +
   scale_color_jco() +
   scale_fill_jco() +
-  theme_light()
+  theme_light() +
+  theme(legend.position = 'top',
+              legend.justification = 'left')
 
-# DROP BAD CELLS
-df.yfp.drop <- df.yfp %>%
-               filter(ID == 'cell1_01_3_04' |  # 405 nm 50%
-                      ID == 'cell1_02_3_04' |
-                      ID == 'cell1_03_3_04' |
-                      ID == 'cell10_01_3_04'|
-                      ID == 'cell4_01_3_04' |
-                      ID == 'cell7_01_3_04' |
-                      ID == 'cell2_01_3_04' |  # 405 nm 75%
-                      ID == 'cell2_02_3_04' |
-                      ID == 'cell2_03_3_04' |
-                      ID == 'cell8_01_3_04' |
-                      ID == 'cell8_02_3_04' |
-                      ID == 'cell8_03_3_04' |
-                      ID == 'cell3_01_3_04' |  # 405 nm 100%
-                      ID == 'cell3_02_3_04' |
-                      ID == 'cell6_01_3_04' |
-                      ID == 'cell9_01_3_04')
+#  facet_grid(rows = vars(cell))
 
-df.yfp.drop.03 <- df.yfp.03 %>%
-                  filter(ID == 'cell7_01_17_03'  |  # 405 nm 20%
-                         ID == 'cell9_01_17_03'  |
-                         ID == 'cell10_01_17_03' |  # 405 nm 50%
-                         ID == 'cell12_01_17_03' |
-                         ID == 'cell13_01_17_03' |
-                         ID == 'cell20_01_17_03') 
-# ALL GOOD CELLS               
-df.yfp.good <- rbind(df.yfp.drop, df.yfp.drop.03)        
 
 # DOSE DEP
 start.time <- 5
 end.time <- 95
 decim.num <- 10
+selected.power <- '100'
 
 w.fun <- function(x, a, b){(exp(-x/a)+b)/max(exp(-x/a)+b)}
 a <- 30
@@ -119,7 +132,7 @@ time.slice.yfp <- df.yfp.good %>%
                          time<=end.time) %>%
                   subset(select = c(ID, time, delta)) %>%
                   group_by(ID) %>%
-                  mutate(norm = delta/max(delta)) %>%
+                  mutate(norm = rel/max(rel)) %>%
                   mutate(dec = HMA(norm, n = 10)) %>%  # w = w.fun(seq(0, length(norm)-1), a, b))
                   rename(yfp = delta)
 time.slice.fluo <- df.fluo %>%
@@ -166,9 +179,9 @@ df.mean.yfp <- subset(df.yfp, select = c(power, time, mask, rel)) %>%
                summarise_all(list(mean, sd), na.rm = TRUE) %>%
                rename(mean = fn1, sd = fn2)
 
-  ggplot() +
+ggplot() +
   geom_line(data = filter(df.mean.yfp, mask == selected.mask),
-            aes(x = time, y = mean, colour = power, fill = power),
+            aes(x = time, y = mean, colour = power),
             size = 1) +
   geom_ribbon(data = filter(df.mean.yfp, mask == selected.mask),
               aes(x = time,
@@ -178,25 +191,27 @@ df.mean.yfp <- subset(df.yfp, select = c(power, time, mask, rel)) %>%
                   colour = as.factor(power)),
               alpha=.2,
               size=0) +
-  geom_line(data = subset(df.fluo, power == c('20', '50')),
-            aes(x = time, y = mean/coeff, colour = power, fill = power),
-            size = 1,
-            linetype = 5,
-            alpha = 0.6) +
   scale_x_continuous(name = 'Time (s)',
                      limits = c(-20, 90),
                      breaks = seq(-100, 100, 5)) +
   scale_y_continuous(name = 'HPCA-YFP ΔF/F0',
-                     breaks = seq(-100, 100, 0.05),
-                     sec.axis = sec_axis(trans = ~.*coeff,
-                                       name = 'Fluo-4 ΔF/F0',
-                                       breaks = seq(-100, 100, 0.5))) +
+                     breaks = seq(-100, 100, 0.05)) +
   labs(title = sprintf("Mask %s mean", selected.mask),
        colour = '405 nm power (%)',
        fill = '405 nm power (%)') +
   scale_color_jco() +
   scale_fill_jco() +
   theme_minimal()
+
+#geom_line(data = subset(df.fluo, power == c('20', '50')),
+#          aes(x = time, y = mean/coeff, colour = power, fill = power),
+#          size = 1,
+#          linetype = 5,
+#          alpha = 0.6) +
+
+#sec.axis = sec_axis(trans = ~.*coeff,
+#                    name = 'Fluo-4 ΔF/F0',
+#                    breaks = seq(-100, 100, 0.5))
 
 # ggsave(sprintf('mean_mask_%s.png', selected.mask, selected.power),
 #        width = 30, height = 15, units = 'cm')
